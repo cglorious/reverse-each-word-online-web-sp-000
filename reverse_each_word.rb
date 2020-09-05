ec5e99f9c29daefbfdef4df281da15a2dc6147de
Catherine Gloria <catherinegloria@gmail.com>
2020-09-05 17:24:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,8 +3,8 @@ def reverse_each_word(string)
   array.collect do |word|
     word.reverse
   end
+  array.join#(" ")
   #take each element, and iterate over to reverse that words
 end
-  array.join(" ")
 hi = "Hello there, and how are you?"
 reverse_each_word(hi)
